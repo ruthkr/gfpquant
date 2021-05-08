f3cdc25c1ddb1867e48f4ad5c0ad5a1533425299
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2021-05-09 00:05:05 +0100

    Defined basic input UI with {shinyMatrix}

--- R/mod_gfp.R
+++ R/mod_gfp.R
@@ -14,8 +14,45 @@ mod_gfp_ui <- function(id) {
     title = "Quantify GFP",
 
     sidebarLayout(
-      sidebarPanel = shiny::sidebarPanel(),
-      mainPanel = mainPanel()
+      sidebarPanel = sidebarPanel(
+        width = 5,
+        shinyMatrix::matrixInput(
+          inputId = ns("mat_std_curve"),
+          label = "Values for standard curve",
+          value = matrix(0, nrow = 3, ncol = 2) %>%
+            `colnames<-`(c("GFP Level", "Fluorescence")),
+          inputClass = "numeric",
+          rows = list(
+            extend = TRUE
+          ),
+          cols = list(
+            names = TRUE
+          )
+        ),
+
+        shinyMatrix::matrixInput(
+          inputId = ns("mat_sample_fluorescence"),
+          label = "Samples' fluorescence values",
+          value = matrix(0, nrow = 3, ncol = 3) %>%
+            `colnames<-`(paste("Sample", 1:3)),
+          inputClass = "numeric",
+          rows = list(
+            extend = TRUE
+          ),
+          cols = list(
+            names = TRUE,
+            editableNames = TRUE,
+            extend = TRUE
+          )
+        )
+
+      ),
+      mainPanel = mainPanel(
+        width = 7,
+
+        h4("Results"),
+        verbatimTextOutput(ns("input_panel_output"))
+      )
     )
   )
 }
@@ -26,6 +63,13 @@ mod_gfp_ui <- function(id) {
 mod_gfp_server <- function(id) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
+
+    output$input_panel_output <- renderPrint({
+      utils::str(list(
+        "Matrix A" = input$mat_std_curve,
+        "Matrix B" = input$mat_sample_fluorescence
+      ))
+    })
   })
 }
 
